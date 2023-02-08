32cbf415c466e640eb62231292fb53780e304a35
sson22 <sophiesbson@gmail.com>
2023-02-09 10:45:42 +1100

    Service completed

---
+++ src/main/java/com/example/springboottodoapplication/repositories/TodoItemRepository.java
@@ -0,0 +1,9 @@
+package com.example.springboottodoapplication.repositories;
+
+import com.example.springboottodoapplication.models.TodoItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TodoItemRepository extends JpaRepository<TodoItem, Long> {
+}
--- src/main/java/com/example/springboottodoapplication/services/TodoItemService.java
+++ src/main/java/com/example/springboottodoapplication/services/TodoItemService.java
@@ -1,8 +1,38 @@
 package com.example.springboottodoapplication.services;
 
+import com.example.springboottodoapplication.models.TodoItem;
+import com.example.springboottodoapplication.repositories.TodoItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.util.Optional;
 
 
 @Service
 public class TodoItemService {
+
+    @Autowired
+    private TodoItemRepository todoItemRepository;
+    public Iterable<TodoItem> getAll(){
+        return todoItemRepository.findAll();
+    }
+
+    public Optional<TodoItem> getById(Long id){
+        return todoItemRepository.findById(id);
+    }
+
+    public TodoItem save(TodoItem todoItem){
+        if(todoItem.getId()==null) {
+            todoItem.setCreatedAt(Instant.now());
+        }
+            todoItem.setUpdatedAt(Instant.now());
+            return todoItemRepository.save(todoItem);
+    }
+
+    public void delete(TodoItem todoItem){
+        todoItemRepository.delete(todoItem);
+
+
+    }
 }
