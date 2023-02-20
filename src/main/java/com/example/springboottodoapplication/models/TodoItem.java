de09d0c39e1bd8d23f0186e10f4e99f1a647c7f1
sson22 <sophiesbson@gmail.com>
2023-02-20 20:17:00 +1100

    add edit/delete items

--- src/main/java/com/example/springboottodoapplication/models/TodoItem.java
+++ src/main/java/com/example/springboottodoapplication/models/TodoItem.java
@@ -1,6 +1,7 @@
 package com.example.springboottodoapplication.models;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,18 +11,26 @@ import java.time.Instant;
 @Getter
 @Setter
 @Entity
-@Table(name="todo_items")
+@Table(name = "todo_items")
 public class TodoItem implements Serializable {
 
-    @Id@GeneratedValue(strategy = GenerationType.AUTO)
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @NotBlank(message = "Description is required")
     private String description;
-    boolean isComplete;
+
+    private Boolean isComplete;
 
     private Instant createdAt;
+
     private Instant updatedAt;
 
-    public String toString(){
-        return String.format("TodoItem{id=%d, description='%s', isComplete='%s', createdAt='%s', updatedAt='%s'}",id, description,isComplete, createdAt, updatedAt );
+    @Override
+    public String toString() {
+        return String.format("TodoItem{id=%d, description='%s', isComplete='%s', createdAt='%s', updatedAt='%s'}",
+                id, description, isComplete, createdAt, updatedAt);
     }
+
 }
--- src/main/java/com/example/springboottodoapplication/services/TodoItemService.java
+++ src/main/java/com/example/springboottodoapplication/services/TodoItemService.java
@@ -8,31 +8,30 @@ import org.springframework.stereotype.Service;
 import java.time.Instant;
 import java.util.Optional;
 
-
 @Service
 public class TodoItemService {
 
     @Autowired
     private TodoItemRepository todoItemRepository;
-    public Iterable<TodoItem> getAll(){
-        return todoItemRepository.findAll();
-    }
 
-    public Optional<TodoItem> getById(Long id){
+    public Optional<TodoItem> getById(Long id) {
         return todoItemRepository.findById(id);
     }
 
-    public TodoItem save(TodoItem todoItem){
-        if(todoItem.getId()==null) {
+    public Iterable<TodoItem> getAll() {
+        return todoItemRepository.findAll();
+    }
+
+    public TodoItem save(TodoItem todoItem) {
+        if (todoItem.getId() == null) {
             todoItem.setCreatedAt(Instant.now());
         }
-            todoItem.setUpdatedAt(Instant.now());
-            return todoItemRepository.save(todoItem);
+        todoItem.setUpdatedAt(Instant.now());
+        return todoItemRepository.save(todoItem);
     }
 
-    public void delete(TodoItem todoItem){
+    public void delete(TodoItem todoItem) {
         todoItemRepository.delete(todoItem);
-
-
     }
+
 }
